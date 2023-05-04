8524a13e0424ce0ed6344f96aff7ed1cb25ae018
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-05 03:27:56 +0530

    Rendering long lists of dummy data efficiently

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
@@ -9,13 +12,32 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('Expeses Graph ....'),
-          Text('Expeses List ....'),
+        children: [
+          const Text('Expeses Graph ....'), // Todo: Bar Graph
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          )
         ],
       ),
     );
