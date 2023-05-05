224015792547af9628b9f05de6d88024244e1995
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-06 02:14:18 +0530

    Adding amount input and button to close the modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,9 +10,14 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-  void _saveTitleInput(String inputTitle) {
-    _enteredTitle = inputTitle;
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,18 +27,35 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
-                onPressed: () => print(_enteredTitle),
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
                 child: const Text('Save Expense'),
-              )
+              ),
             ],
           )
         ],
