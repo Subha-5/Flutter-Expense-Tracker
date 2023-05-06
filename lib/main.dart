2f2d16472615774aecae6e1d67ada4cbcf3930a2
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-06 14:48:48 +0530

    Date picker

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
+    ),
+  );
 }
