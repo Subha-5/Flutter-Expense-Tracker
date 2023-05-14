8c02f2f37aa36b4063c5c8db30e2e461de2f9062
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-15 02:43:31 +0530

    updating UI based on available space

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -13,10 +13,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
     runApp(
       MaterialApp(
         darkTheme: ThemeData.dark().copyWith(
@@ -67,5 +67,5 @@ void main() {
         home: const Expenses(),
       ),
     );
-  });
+  // });
 }
